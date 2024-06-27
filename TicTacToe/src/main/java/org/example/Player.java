fa256067b28e0894a0872982796cfb1014e8555b
Marko Paradzik <markoparadzik@Air-von-Marko.fritz.box>
2024-06-27 21:56:02 +0200

    Implement feature: make a move

---
+++ TicTacToe/src/main/java/org/example/Player.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Player {
+    private char marker;
+
+    public Player(char marker) {
+        this.marker = marker;
+    }
+
+    public char getMarker() {
+        return marker;
+    }
+}
+
