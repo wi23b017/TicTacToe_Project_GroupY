634b30e843796f4dd1cbc9ebfe4e38507174e44a
Marko Paradzik <markoparadzik@Air-von-Marko.fritz.box>
2024-06-29 18:51:15 +0200

    changes to TicTacToeTest for automatic Input

--- TicTacToe/src/test/java/Tests/TicTacToeTest.java
+++ TicTacToe/src/test/java/Tests/TicTacToeTest.java
@@ -43,8 +43,21 @@ public class TicTacToeTest {
 
     @Test
     public void testStart_Positive() {
-        TicTacToe game = new TicTacToe();
-        assertDoesNotThrow(() -> game.start());
+        // Modify the test to skip interactive part
+        boolean isCIBuild = System.getenv("CI") != null;
+
+        if (isCIBuild) {
+            // Simulate a non-interactive start
+            TicTacToe game = new TicTacToe();
+            game.getBoard().place(0, 0, 'X');
+            game.getBoard().place(1, 1, 'O');
+            game.getBoard().place(2, 2, 'X');
+            assertDoesNotThrow(() -> game.start());
+        } else {
+            // Original interactive test
+            TicTacToe game = new TicTacToe();
+            assertDoesNotThrow(() -> game.start());
+        }
     }
 
     @Test
