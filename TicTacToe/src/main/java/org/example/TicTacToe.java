734c99203fb5f3ed381c0c55783c1d076754c8a6
Marko Paradzik <markoparadzik@Air-von-Marko.fritz.box>
2024-06-29 19:01:45 +0200

    changes to TicTacToe for automatic Input

--- TicTacToe/src/main/java/org/example/TicTacToe.java
+++ TicTacToe/src/main/java/org/example/TicTacToe.java
@@ -98,5 +98,18 @@ public class TicTacToe {
     public Board getBoard() {
         return board;
     }
+
+    public void simulateGame() {
+        // Simulate some moves
+        board.place(0, 0, 'X');
+        board.place(0, 1, 'O');
+        board.place(0, 2, 'X');
+        board.place(1, 0, 'O');
+        board.place(1, 1, 'X');
+        board.place(1, 2, 'O');
+        board.place(2, 0, 'X');
+        board.place(2, 1, 'O');
+        board.place(2, 2, 'X');
+    }
 }
 
