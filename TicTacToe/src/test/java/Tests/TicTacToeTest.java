2cc777dde42e371fec8450bb02c116ba3487fe92
Marko Paradzik <markoparadzik@Air-von-Marko.fritz.box>
2024-06-29 19:00:40 +0200

    changes to TicTacToeTest for automatic Input

--- TicTacToe/src/test/java/Tests/TicTacToeTest.java
+++ TicTacToe/src/test/java/Tests/TicTacToeTest.java
@@ -52,11 +52,11 @@ public class TicTacToeTest {
             game.getBoard().place(0, 0, 'X');
             game.getBoard().place(1, 1, 'O');
             game.getBoard().place(2, 2, 'X');
-            assertDoesNotThrow(() -> game.start());
+            assertDoesNotThrow(() -> game.simulateGame());
         } else {
             // Original interactive test
             TicTacToe game = new TicTacToe();
-            assertDoesNotThrow(() -> game.start());
+            assertDoesNotThrow(() -> game.simulateGame());
         }
     }
 
